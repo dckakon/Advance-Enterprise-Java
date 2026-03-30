3fd33c37d2610f5bd86b15e8de2679742ff55af2
Debobrata Chakraborty <61970194+dckakon@users.noreply.github.com>
2026-03-30 21:11:02 +0600

    file handling 2

--- FileHandlingClass.java
+++ FileHandlingClass.java
@@ -1,34 +1,20 @@
 import java.io.File;
-import java.io.IOException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileHandlingClass {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        File myFile = new File("javaClass.txt"); // objects
+        myFile.createNewFile();
 
-            File myFile= new File("myfile.doc");
-            File dir= new File("MyFolder");
+        FileWriter myWriter= new FileWriter("javaClass.txt");
+        myWriter.write("Student ID:121212");
+        myWriter.close();
 
-       try {
-           myFile.createNewFile();
-           dir.mkdir();
-
-           FileWriter myWriter = new FileWriter("myFile.doc");
-           myWriter.write("hello everyone");
-           myWriter.close();
-
-           Scanner myReader = new Scanner(myFile);
-           String data = myReader.nextLine();
-
-           myFile.delete();
-           System.out.println(data);
-
-       } catch (IOException e) {
-           System.out.print("An error occurred");
-       }
-
-
-
+        Scanner myReader= new Scanner(myFile);
+        String data = myReader.nextLine();
+        System.out.println(data);
     }
 }
